116a85a377a66ba50806a8791eb55b66b24f3182
quixote15 <santos2tiago@gmail.com>
2021-10-28 23:23:17 -0300

    feature: deploy first catch block

--- handler.js
+++ handler.js
@@ -1,7 +1,40 @@
-'use strict';
+const AWS = require("aws-sdk");
+const StepFunction = new AWS.StepFunctions();
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient({ region: "us-east-1" });
 
-module.exports.checkInventory = async (event) => {
-  return 'Stocks available'
+const isBookAvailable = (book, quantity) => {
+  return (book.quantity - quantity) > 0
+}
+
+module.exports.checkInventory = async ({ bookId, quantity }) => {
+  try {
+    let params = {
+      TableName: 'bookTable',
+      KeyConditionExpression: 'bookId = :bookId',
+      ExpressionAttributeValues: {
+        ':bookId': bookId
+      }
+    };
+    let result = await DocumentClient.query(params).promise();
+    let book = result.Items[0];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The book is out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+  } catch (e) {
+    if (e.name === 'BookOutOfStock') {
+      throw e;
+    } else {
+      let bookNotFoundError = new Error(e);
+      bookNotFoundError.name = 'BookNotFound';
+      throw bookNotFoundError;
+    }
+  }
 };
 
 
